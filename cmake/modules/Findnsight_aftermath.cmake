3473bdee2fc8f389cebd6b8d80975b11e82b7bae
Silverlan <silverlann@gmail.com>
2025-07-01 14:46:17 +0200

    build: update cmake script

--- cmake/modules/Findnsight_aftermath.cmake
+++ cmake/modules/Findnsight_aftermath.cmake
@@ -16,9 +16,20 @@ find_library(${PCK}_LIBRARY
     ${PRAGMA_DEPS_DIR}/nsight_aftermath/lib
 )
 
+set(REQ_VARS ${PCK}_LIBRARY ${PCK}_INCLUDE_DIR)
+if(WIN32)
+  find_file(
+    ${PCK}_RUNTIME
+    NAMES GFSDK_Aftermath_Lib.x64.dll
+    HINTS
+      ${PRAGMA_DEPS_DIR}/nsight_aftermath/bin
+  )
+  set(REQ_VARS ${REQ_VARS} ${PCK}_RUNTIME)
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${PCK}
-  REQUIRED_VARS ${PCK}_LIBRARY ${PCK}_INCLUDE_DIR
+  REQUIRED_VARS ${REQ_VARS}
 )
 
 if(${PCK}_FOUND)
