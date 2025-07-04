eede01e537f9a1d9341b874bf2210010ecffdb5f
Silverlan <silverlann@gmail.com>
2025-07-04 20:05:12 +0200

    build: disable build if prebuilt binaries are used

--- build_scripts/setup.py
+++ build_scripts/setup.py
@@ -4,20 +4,22 @@ from sys import platform
 import tarfile
 from pathlib import Path
 import urllib.request
+import config
 
-os.chdir(deps_dir)
-
-########## NVIDIA Nsight Aftermath SDK ##########
-nsight_aftermath_root = deps_dir +"/NVIDIA_Nsight_Aftermath_SDK_2024.3.0.24312"
-if not Path(nsight_aftermath_root).is_dir():
-	print_msg("NVIDIA Nsight Aftermath SDK not found. Downloading...")
-	mkpath(nsight_aftermath_root)
-	os.chdir(nsight_aftermath_root)
-	if platform == "win32":
-		http_extract("https://developer.nvidia.com/downloads/assets/tools/secure/nsight-aftermath-sdk/2024_3_0/windows/NVIDIA_Nsight_Aftermath_SDK_2024.3.0.24312.zip")
-	else:
-		http_extract("https://developer.nvidia.com/downloads/assets/tools/secure/nsight-aftermath-sdk/2024_3_0/linux/NVIDIA_Nsight_Aftermath_SDK_2024.3.0.24312.tgz",format="tar.gz")
+if build_all:
 	os.chdir(deps_dir)
 
-copy_prebuilt_binaries(nsight_aftermath_root +"/lib/x64/", "nsight_aftermath")
-copy_prebuilt_headers(nsight_aftermath_root +"/include", "nsight_aftermath")
+	########## NVIDIA Nsight Aftermath SDK ##########
+	nsight_aftermath_root = deps_dir +"/NVIDIA_Nsight_Aftermath_SDK_2024.3.0.24312"
+	if not Path(nsight_aftermath_root).is_dir():
+		print_msg("NVIDIA Nsight Aftermath SDK not found. Downloading...")
+		mkpath(nsight_aftermath_root)
+		os.chdir(nsight_aftermath_root)
+		if platform == "win32":
+			http_extract("https://developer.nvidia.com/downloads/assets/tools/secure/nsight-aftermath-sdk/2024_3_0/windows/NVIDIA_Nsight_Aftermath_SDK_2024.3.0.24312.zip")
+		else:
+			http_extract("https://developer.nvidia.com/downloads/assets/tools/secure/nsight-aftermath-sdk/2024_3_0/linux/NVIDIA_Nsight_Aftermath_SDK_2024.3.0.24312.tgz",format="tar.gz")
+		os.chdir(deps_dir)
+
+	copy_prebuilt_binaries(nsight_aftermath_root +"/lib/x64/", "nsight_aftermath")
+	copy_prebuilt_headers(nsight_aftermath_root +"/include", "nsight_aftermath")
